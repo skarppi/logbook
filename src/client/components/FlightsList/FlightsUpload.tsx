15570c1ee774015ff637450c96d8fd36f9211a33
Juho Kolehmainen <juho.kolehmainen@iki.fi>
2018-12-29 23:37:12 +0200

    Drag and drop multiple files.

--- src/client/components/FlightsList/FlightsUpload.tsx
+++ src/client/components/FlightsList/FlightsUpload.tsx
@@ -1,19 +1,13 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  Grid,
-  List,
-  ListItem
-} from "@material-ui/core";
+import { Card, CardContent, CardHeader } from "@material-ui/core";
 import * as React from "react";
-import { NavLink, Route } from "react-router-dom";
-import { IFlight } from "../../../shared/IFlight";
 import { uploadFlightsAPI } from "../../utils/api-facade";
-import { Flight } from "../Flight/Flight";
+import classNames from "classnames";
+import Dropzone from "react-dropzone";
+
+const css = require("./FlightsUpload.css");
 
 interface IState {
-  file: File | null;
+  files: File[];
   loaded: number;
 }
 
@@ -21,40 +15,67 @@ export class FlightsUpload extends React.Component<any, IState> {
   constructor(props) {
     super(props);
     this.state = {
-      file: null,
+      files: [],
       loaded: 0
     };
-    this.handleselectedFile = this.handleselectedFile.bind(this);
     this.handleUpload = this.handleUpload.bind(this);
+    this.handleDrop = this.handleDrop.bind(this);
   }
 
   public render() {
+    const files = this.state.files.map((file, index) => (
+      <li key={file.name}>
+        {file.name} - {file.size} bytes
+      </li>
+    ));
+
     return (
       <Card>
         <CardHeader title="Upload New Flight" />
         <CardContent>
-          <input
-            type="file"
-            name="flight"
-            onChange={this.handleselectedFile}
-            multiple
-          />
-          <button onClick={this.handleUpload}>Upload</button>
-          <div> {this.state.loaded} %</div>
+          <Dropzone onDrop={this.handleDrop}>
+            {({ getRootProps, getInputProps, isDragActive }) => {
+              return (
+                <div
+                  {...getRootProps()}
+                  className={classNames(css.dropzone, {
+                    "dropzone--isActive": isDragActive
+                  })}
+                >
+                  <input {...getInputProps()} />
+                  {isDragActive ? (
+                    <p>Drop files here...</p>
+                  ) : (
+                    <div>
+                      <p>
+                        Drag and drop log files or <u>click</u>
+                      </p>
+                    </div>
+                  )}
+                  <h4 hidden={this.state.files.length === 0}>Files</h4>
+                  <ul>{files}</ul>
+                </div>
+              );
+            }}
+          </Dropzone>
+
+          <div hidden={this.state.files.length === 0}>
+            <button onClick={this.handleUpload}>Upload</button>
+            {this.state.loaded} %
+          </div>
         </CardContent>
       </Card>
     );
   }
 
-  private handleselectedFile(event) {
-    if (event.target.files) {
-      this.setState({ file: event.target.files[0], loaded: 0 });
-    }
+  private handleDrop(files) {
+    console.log("handleDrop!", files);
+    this.setState({ files: files, loaded: 0 });
   }
 
   private handleUpload() {
     const data = new FormData();
-    data.append("flight", this.state.file, this.state.file.name);
+    this.state.files.forEach(file => data.append("flight", file, file.name));
 
     uploadFlightsAPI(data, (progressEvent: any) => {
       this.setState({
