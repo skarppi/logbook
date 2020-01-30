94540527ff28c2a2e5f7767d5c5f9dafce6b4193
Juho Kolehmainen <juho.kolehmainen@iki.fi>
2020-01-31 00:26:11 +0200

    Fix sql config.

--- sql/2-locations.sql
+++ sql/2-locations.sql
@@ -11,15 +11,12 @@ insert into LOCATIONS (name)
 SELECT f.notes->>'location' as name
     from flights f
     where CHAR_LENGTH( f.notes->>'location' ) > 0
-    group by name
+    group by name;
 
-ALTER TABLE flights ADD COLUMN location integer REFERENCES locations(id);
+ALTER TABLE flights ADD COLUMN location_id integer REFERENCES locations(id);
 
-comment on constraint "flights_location_fkey" on "flights" is
-  E'@foreignFieldName locationById';
-
-UPDATE flights set location = (select id from locations where name = notes->>'location');
+UPDATE flights set location_id = (select id from locations where name = notes->>'location');
 SELECT f.notes->>'location' as name
     from flights f
     where CHAR_LENGTH( f.notes->>'location' ) > 0
-    group by name
+    group by name;
