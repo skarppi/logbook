81c9f96ea1ab9c6d1fdc2acc829a98f060dcfb69
Juho Kolehmainen <juho.kolehmainen@iki.fi>
2020-05-16 12:49:07 +0300

    Appbar shows icons.

--- client/src/app/Header/Header.tsx
+++ client/src/app/Header/Header.tsx
@@ -4,49 +4,59 @@ import Toolbar from '@material-ui/core/Toolbar';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { LinkProps } from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+import HomeIcon from '@material-ui/icons/Home';
+import FlightsIcon from '@material-ui/icons/List';
+import PlanesIcon from '@material-ui/icons/AirplanemodeActive';
+import BatteriesIcon from '@material-ui/icons/BatteryChargingFull';
+import LocationIcon from '@material-ui/icons/LocationOn';
+import UploadIcon from '@material-ui/icons/CloudUpload';
+
 
 const link = (url: string) => React.forwardRef<HTMLAnchorElement, Partial<LinkProps>>((props, ref) => <Link to={url} {...props} ref={ref} />);
 
-export const Header = () => (
-  <AppBar position='static' color='transparent'>
+const ToolBarButton = ({ url, text, Icon }) => {
+  const css = useStyles();
+
+  return <Button
+    color='primary'
+    component={link(url)}
+  >
+    <Icon className={css.icon} />
+    <Typography variant='button' className={css.text}>{text}</Typography>
+  </Button>;
+};
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(1),
+  },
+  text: {
+    [theme.breakpoints.down('xs')]: {
+      display: 'none',
+    }
+  }
+}
+));
+
+export const Header = () => {
+  const css = useStyles();
+
+  return <AppBar position='static' color='transparent'>
     <Toolbar>
-      <Button color='primary' component={link('/')}>
-        Home
-      </Button>
-      <Button
-        color='primary'
-        component={link('/flights')}
-      >
-        Flights
-      </Button>
+      <ToolBarButton url='/' text='Home' Icon={HomeIcon} />
 
-      <Button
-        color='primary'
-        component={link('/planes')}
-      >
-        Planes
-      </Button>
+      <ToolBarButton url='/flights' text='Batteries' Icon={BatteriesIcon} />
 
-      <Button
-        color='primary'
-        component={link('/batteries')}
-      >
-        Batteries
-      </Button>
+      <ToolBarButton url='/planes' text='Planes' Icon={PlanesIcon} />
 
-      <Button
-        color='primary'
-        component={link('/locations')}
-      >
-        Locations
-      </Button>
+      <ToolBarButton url='/batteries' text='Batteries' Icon={BatteriesIcon} />
 
-      <Button
-        color='primary'
-        component={link('/upload')}
-      >
-        Upload
-      </Button>
+      <ToolBarButton url='/locations' text='Locations' Icon={LocationIcon} />
+
+      <ToolBarButton url='/upload' text='Upload' Icon={UploadIcon} />
     </Toolbar>
-  </AppBar>
-);
+  </AppBar>;
+};
