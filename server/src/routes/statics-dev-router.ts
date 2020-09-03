203ee51781dc027f233f4b472967626211401fe0
Juho Kolehmainen <juho.kolehmainen@iki.fi>
2020-09-03 17:30:26 +0300

    Update server libraries.

--- server/src/routes/statics-dev-router.ts
+++ server/src/routes/statics-dev-router.ts
@@ -1,4 +1,4 @@
-import * as proxy from 'http-proxy-middleware';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import { Router } from 'express';
 import { PUBLIC_PATH } from '../config';
 
@@ -6,14 +6,14 @@ export function staticsDevRouter() {
   const router = Router();
 
   // All the assets are hosted by Webpack on localhost:3001 (Webpack-dev-server)
-  router.use(['/public', '/sockjs-node'], proxy(
+  router.use(['/public', '/sockjs-node'], createProxyMiddleware(
     {
       target: 'http://localhost:3001/',
       ws: true
     }));
 
   // Any route should render the web app html (hosted by by Webpack-dev-server)
-  router.use('**', proxy(
+  router.use('**', createProxyMiddleware(
     {
       target: 'http://localhost:3001/',
       pathRewrite: path => `${PUBLIC_PATH}/public/index.html`,
